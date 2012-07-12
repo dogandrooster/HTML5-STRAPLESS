78b7889912ba104ecac789fdd18f724e9be5b6e3
Mark Joseph Rivera <imarkdesigns@gmail.com>
2012-07-12 10:04:27 +0800

    welcome to html5 LESS Bootstrap

---
+++ index.php
@@ -0,0 +1,43 @@
+<?
+	#Class pattern for Main Container 
+	$pattern = 'home';
+?>
+<? include 'manager/pi/home.php'; ?>
+
+
+ 
+<? startblock('content') ?>
+<article id=box>
+	<div class=page-header>
+		<h1>HI, IM MARK RIVERA <small>and welcome</small></h1>
+	</div>
+	<div class='hero-unit clearfix'>
+		<h1>HTML5 + BOOTSTRAP</h1>
+		<p>This is my latest HTML5 Frameworks with Bootstrap integration.</p>
+		<p>
+		  <a class="btn btn-primary" href="#">
+		  	<i class="icon-wrench icon-white"></i> Launch HTML5 Introduction
+		  </a>
+
+			<div class=btn-toolbar>
+			  <div class="btn-group">
+			    <button class="btn btn-success">Success</button>
+			    <button class="btn btn-info">Information</button>
+			    <button class="btn btn-warning">Warning</button>
+			    <button class="btn btn-inverse">Inverse</button>
+			    <button class="btn btn-danger">Danger</button>
+			  </div>
+			</div>
+		</p> 
+	</div>
+</article>
+<? endblock(); ?>
+
+
+ 
+<? startblock('headercodes') ?>
+<link rel="stylesheet" href="<?=$root?>assets/css/home.css">
+<? endblock(); ?>
+
+<? startblock('extracodes') ?>
+<? endblock(); ?>
