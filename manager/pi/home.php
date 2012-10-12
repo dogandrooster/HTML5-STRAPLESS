3ae3e1c019a4e300c9dcde61982a1c998f5bee60
Mark Joseph Rivera <imarkdesigns@gmail.com>
2012-10-12 09:19:36 +0800

    Updated to the latest Bootstrap Framework

--- manager/pi/home.php
+++ manager/pi/home.php
@@ -55,10 +55,10 @@
 <?=emptyblock('extracodes');?>
 <script>
 	//GOOGLE ANALYTIC SCRIPT
-	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-	s.parentNode.insertBefore(g,s)}(document,'script'));
+	//var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	//(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	//g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	//s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
 </body>
 </html>
